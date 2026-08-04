27a285c009ff93253a34fcc3ce0ef192c8c8a70f
tcuenca1 <leonardocuencaguzman@gmail.com>
2026-08-04 00:08:51 -0500

    fix: despliegue a railway v2

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://gestionmedico-production-9e80.up.railway.app/api',
+  apiBaseUrl: 'https://gestionmedico-production-5efa.up.railway.app/api',
 };
